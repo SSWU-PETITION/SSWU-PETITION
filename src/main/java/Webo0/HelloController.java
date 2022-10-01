2025eaebc0bd7309e4dd8a7be5892c624295f628
shinyeong-gwak <20202160@sungshin.ac.kr>
2022-10-01 19:29:51 +0900

    - index.html 최초 페이지 수정 - 스프링부트 입문편 실습 중

---
+++ src/main/java/Webo0/HelloController.java
@@ -0,0 +1,29 @@
+package Webo0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") //"hello"페이지가 만들어졌을때 페이지 경로가 뭐라 뜰지
+    public String hello(Model model) { //요게 view resolver인 듯
+        model.addAttribute("data", "hello!!"); //html에서 원하는 {data}값에 밸류를 넣는 명령어
+        return "hello"; // 여기서 hello.html을 다시 불러줌
+    }
+    @GetMapping("hello-mvc")//주소 mvc로 받아서
+    public String helloMvc(@RequestParam("name") String name, Model model) { //@가 name에 주소창에서 채울지 물어보는 함수인듯
+        model.addAttribute("name", name); // name에다가 주소창에서 변수를 채워줘야함
+        return "hello-template"; //실제 경로인 temp 틀기
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody//view Resolver로 html을 불러오지 않음
+    public String helloString(@RequestParam("name") String name) {
+        return "halo " + name; //hello-string이라는 홈페이지에 들어갔을 때 띄울 말, html없이 가능
+
+    }
+
+}
---
+++ src/main/java/Webo0/Webo0Application.java
@@ -0,0 +1,13 @@
+package Webo0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webo0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webo0Application.class, args);
+	}
+
+}
